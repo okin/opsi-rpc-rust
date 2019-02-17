41e9cbc7161a074094e3712834daeb0a930b4efb
Niko Wenselowski <der@nik0.de>
2019-02-17 14:13:25 +0100

    First version.

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+extern crate reqwest;
+
+use std::collections::HashMap;
+use std::env;
+
+fn help() {
+	println!("opsi-rpc <address> <username> <password>");
+}
+
+
+fn parse_cli() -> [String; 3] {
+	let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+	    4 => {
+	        let address = &args[1];
+	        let username = &args[2];
+	        let password = &args[3];
+
+	        [address.to_string(), username.to_string(), password.to_string()]
+	    },
+	    _ => {
+	        // show a help message
+	        help();
+	        panic!("invalid arguments");
+    	}
+    }
+}
+
+fn post(address: &str, username: &str, password: &str) -> Result<(), Box<std::error::Error>> {
+ 	let mut jrpc = HashMap::new();
+ 	jrpc.insert("id", "1");
+ 	jrpc.insert("method", "backend_info");
+ 	jrpc.insert("params", "");
+
+ 	let client = reqwest::Client::new();
+	let mut response = client.post(address)
+ 		.basic_auth(username, Some(password))
+ 		.json(&jrpc)
+ 		.send()?;
+ 	println!("Headers: {:#?}", response);
+
+ 	let content = response.text()?;
+ 	println!("Body: {:#?}", content);
+
+ 	Ok(())
+}
+
+fn main() {
+	let args: [String; 3] = parse_cli();
+
+	let addr = &args[0];
+	let username = &args[1];
+	let password = &args[2];
+
+	println!("Connecting to {} as {}", addr, username);
+
+	let _result = post(&addr, &username, &password);
+}
